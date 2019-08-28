8587a7843a9fe541929489083562164fb0e42fa7
extrahappyperson <ht.uzpharma@gmail.com>
2019-08-28 14:12:38 +0200

    strength and endurance

--- src/health/workouts/components/workout-form/workout-form.component.ts
+++ src/health/workouts/components/workout-form/workout-form.component.ts
@@ -24,13 +24,50 @@ import { Workout } from '../../../shared/services/workouts/workouts.services';
                 
                 <label>
                     <h3>Type</h3>
-                    
+
                     <workout-type
                         formControlName="type">
                     </workout-type>
                 </label>
+                </div>
+
+                <div class="workout-form__details">
+                    <div *ngIf="form.get('type').value === 'strength'">
+                        <div 
+                            class="workout-form__fields"
+                            formGroupName="strength">
+                            <label>
+                                <h3>Reps</h3>
+                                <input type="number" formControlName="reps">
+                            </label>
+                            <label>
+                                <h3>Sets</h3>
+                                <input type="number" formControlName="sets">
+                            </label>
+                            <label>
+                                <h3>Weight <span>(kg)</span></h3>
+                                <input type="number" formControlName="weight">
+                            </label>
+                        </div>
+                    </div>
+
+                    <div *ngIf="form.get('type').value === 'endurance'">
+                        <div 
+                            class="workout-form__fields"
+                            formGroupName="endurance">
+                        <label>
+                            <h3>Distance <span>(km)</span></h3>
+                            <input type="number" formControlName="distance">
+                        </label>
+                        <label>
+                            <h3>Duration <span>(minutes)</span></h3>
+                            <input type="number" formControlName="duration">
+                        </label>
 
 
+                        </div>
+                    </div>
+                
                 </div>
 
                 <div class="workout-form__submit">
@@ -106,7 +143,16 @@ export class WorkoutFormComponent implements OnChanges {
     
     form = this.fb.group({
         name: ['', Validators.required], 
-        type: 'strength'
+        type: 'strength', 
+        strength: this.fb.group({
+            reps:0, 
+            sets:0, 
+            weight:0
+        }), 
+        endurance: this.fb.group({
+            distance:0, 
+            duration:0
+        })
         
     })
 
